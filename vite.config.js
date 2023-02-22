a7335401c033e1bb73debfcf015b16697d68a50e
greenbabyborn <greenbabyborn.com>
2023-02-23 00:41:15 +0300

    fix: workflows

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 "resources/sass/app.scss",
                 "resources/js/app.js",
+                "resources/js/script.js",
                 "resources/admin/plugins/jquery/jquery.min.js",
                 "resources/admin/plugins/bootstrap/js/bootstrap.bundle.min.js",
                 "resources/admin/dist/js/adminlte.min.js",
